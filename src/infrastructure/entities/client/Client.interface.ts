64d097dcb81db1fb951753a39a070be002eb3a05
Carlos Garcia <carlosgm128@gail.com>
2022-09-24 22:49:03 -0400

    added client schema and interface

---
+++ src/infrastructure/entities/client/Client.interface.ts
@@ -0,0 +1,12 @@
+export interface IClient {
+  address: string,
+  cardCode: number,
+  createAt: string,
+  active: boolean,
+  email: string,
+  phone: string,
+  points: number,
+  updateAt: Date,
+  visits: number,
+  id: number
+}
