80c425167314e07e37c1a75156769e3b5811c62c
Raul Luis <soyrauluis@gmail.com>
2022-11-03 02:00:52 -0400

    Use async/await

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -8,30 +8,30 @@ import { UpdateProductDto } from './dto/update-product.dto';
 export class ProductService {
   constructor(private prisma: PrismaService) {}
 
-  create(createProductDto: CreateProductDto) {
-    return this.prisma.product.create({ data: { ...createProductDto } });
+  async create(createProductDto: CreateProductDto) {
+    return await this.prisma.product.create({ data: { ...createProductDto } });
   }
 
-  findAll() {
-    return this.prisma.product.findMany();
+  async findAll() {
+    return await this.prisma.product.findMany();
   }
 
-  findOne(id: string) {
-    return this.prisma.product.findUniqueOrThrow({ where: { id } });
+  async findOne(id: string) {
+    return await this.prisma.product.findUniqueOrThrow({ where: { id } });
   }
 
-  update(id: string, updateProductDto: UpdateProductDto) {
-    return this.prisma.product.update({ where: { id }, data: { ...updateProductDto } });
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    return await this.prisma.product.update({ where: { id }, data: { ...updateProductDto } });
   }
 
-  remove(id: string) {
+  async remove(id: string) {
     try {
-      const deleted = this.prisma.product.delete({ where: { id } });
+      const deleted = await this.prisma.product.delete({ where: { id } });
       return deleted;
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2025') {
-          throw new NotFoundException('Product not found');
+          throw new ForbiddenException('Product not found');
         }
       }
       throw error;
