9f328e4321692ebb910779837306b99d419da858
Raul Luis <soyrauluis@gmail.com>
2022-11-03 00:56:16 -0400

    Remove unused files and configure prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, ProductModule],
 })
 export class AppModule {}
