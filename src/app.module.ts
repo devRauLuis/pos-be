7a17aeafbac5d2da73832e0b57ee360e138df667
Jhomiguel Florian <jhomiguelflorianmendez20@gmail.com>
2022-11-04 23:19:53 -0400

    create auth module with jwt base config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, ProductModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, ProductModule, AuthModule, UserModule],
 })
 export class AppModule {}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: { expiresIn: configService.get<string>('JWT_TOKEN_EXPIRATION_TIME') },
+        };
+      },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
