7948ad532d4b6cb6a9eb49655bce4be3614f34c0
Raul Luis <soyrauluis@gmail.com>
2022-11-05 16:26:07 -0400

    Use argon instead of bcrypt Use config to access env Restrict user creation by role

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,12 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LogUserDto } from './dto/log-user.dto';
-import { LocalAuthGuard } from './guard/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
-  @Post()
-  create(@Body() createAuthDto: LogUserDto) {
-    return this.authService.login(createAuthDto);
+  @Post('/login')
+  create(@Body() payload: any) {
+    return this.authService.login(payload);
   }
 }
